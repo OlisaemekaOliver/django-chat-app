18fd2ff0862a217a98ab6d88a41713716c84b940
Olisa <oliverolisaemeka8@gmail.comgit config --global user.name Olivergit config user.email oliverolisaemeka8@gmail.com>
2025-12-05 09:08:11 +0100

    Fix logout

--- chat/chatapp/urls.py
+++ chat/chatapp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("", home, name="home"),
     path("chat/<int:conversation_id>/", chat_view, name="chat_view"),
     path("login/", auth_views.LoginView.as_view(template_name="chatapp/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     path("signup/", signup, name="signup"),
 ]
